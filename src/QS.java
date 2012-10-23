7c534e2967b7a776d3e1bce64cf9cafe247e6b80
Lucas <thezorg@hotmail.com>
2012-10-23 19:16:37 +0200

    Quadratic Sieve refinements

--- src/QS.java
+++ src/QS.java
@@ -9,8 +9,8 @@ public class QS {
 	private final BigInteger ZERO = BigInteger.ZERO;
 	private final BigInteger ONE = BigInteger.ONE;
 	private final BigInteger TWO = ONE.add(ONE);
-	private final int bitChunkSize = 30;
-	private static final int PRIME_BASE = 1500;
+	private final int flattenFactor = 30;
+	private static final int PRIME_BASE = 1000;
 
 	// Fields
 	private BigInteger[] bigPrimes;
@@ -100,7 +100,8 @@ public class QS {
 
 		// Congruences
 		for (int i = 1; i < PRIME_BASE; i++) {
-			long[] smooth = findSmoothNumbers(primes[i], N.mod(bigPrimes[i]).intValue());
+			// Finds smooth numbers over the factor base.
+			long[] smooth = findQxPolynomials(primes[i], N.mod(bigPrimes[i]).intValue());
 			smoothNumbers[i][0] = (int) smooth[0];
 			smoothNumbers[i][1] = (int) smooth[1];
 		}
@@ -113,12 +114,13 @@ public class QS {
 
 		// This will be the "random" numbers squared modulo
 		fs = new BigInteger[iterationLimit + 2];
+
 		logs = new double[iterationLimit + 2];
 
 		int direction = 0;
 		int offset = 0;
 		do {
-
+			// Decides parameters for finding the next s/fs-values.
 			switch (direction) {
 			case 0:
 				direction = 1;
@@ -134,6 +136,7 @@ public class QS {
 				break;
 			}
 
+			// Cleans the s/vs/logs-arrays.
 			for (int i = 0; i < iterationLimit; i++) {
 				s[i] = null;
 				fs[i] = null;
@@ -152,19 +155,23 @@ public class QS {
 				// The M-value % p + offset.
 				int mInt = m.mod(bigPrimes[i]).intValue() + offset;
 
+				// Checks if the congruence is solved.
 				if (smoothNumbers[i][0] >= 0) {
 					int loop = (int) ((smoothNumbers[i][0] - mInt) % p);
 					if (loop < 0)
 						loop += p;
+					// Sieves in the logs-array.
 					while (loop < iterationLimit) {
 						logs[loop] += logp;
 						loop += p;
 					}
 				}
+				// Checks if the congruence is solved.
 				if (smoothNumbers[i][1] >= 0) {
 					int loop = (int) ((smoothNumbers[i][1] - mInt) % p);
 					if (loop < 0)
 						loop += p;
+					// Sieves in the logs-array.
 					while (loop < iterationLimit) {
 						logs[loop] += logp;
 						loop += p;
@@ -172,12 +179,16 @@ public class QS {
 				}
 			}
 
+			// The value that a sieved element must have to have been hit enough
+			// times.
 			double TARGET = (double) (Math.log(m.doubleValue()) + Math.log(iterationLimit) - primeLogs[PRIME_BASE - 1]);
 
 			for (int i = 0; i < iterationLimit; i++) {
+				// Checks the sieve for target value (if it has been hit by the
+				// sieve enough times).
 				if (logs[i] > TARGET) {
+					// Calculates s[i] and fs[i] if the sieve hit this number.
 					s[i] = BigInteger.valueOf(i + offset).add(m);
-					// Quadratic Function
 					fs[i] = s[i].multiply(s[i]).subtract(N);
 				}
 			}
@@ -187,14 +198,15 @@ public class QS {
 				int prime = primes[i];
 				int mInt = m.mod(bigPrimes[i]).intValue() + offset;
 
+				// Checks if the congruence is solved.
 				if (smoothNumbers[i][0] >= 0) {
 					int loop = (int) ((smoothNumbers[i][0] - mInt) % prime);
 					if (loop < 0)
 						loop += prime;
 					while (loop < iterationLimit) {
 						if (logs[loop] > TARGET) {
-							removeHighestPower(loop, bigPrimes[i]);
-							if (decomposedCount >= PRIME_BASE + bitChunkSize)
+							divideByPower(loop, bigPrimes[i]);
+							if (decomposedCount >= PRIME_BASE + flattenFactor)
 								break;
 						}
 						loop += prime;
@@ -204,26 +216,34 @@ public class QS {
 					int loop = (int) ((smoothNumbers[i][1] - mInt) % prime);
 					if (loop < 0)
 						loop += prime;
+					// Going through the sieve to decompose numbers.
 					while (loop < iterationLimit) {
 						if (logs[loop] > TARGET) {
-							removeHighestPower(loop, bigPrimes[i]);
-							if (decomposedCount >= PRIME_BASE + bitChunkSize)
+							// Divides f[loop] with the current prime as long as
+							// it's possible. If it ends up with 1, it is fully
+							// decomposed and added to the vector.
+							divideByPower(loop, bigPrimes[i]);
+							// Enough numbers are decomposed, ends the looping.
+							if (decomposedCount >= PRIME_BASE + flattenFactor)
 								break;
 						}
 						loop += prime;
 					}
 				}
-				if (decomposedCount >= PRIME_BASE + bitChunkSize)
+				if (decomposedCount >= PRIME_BASE + flattenFactor)
 					break;
 			}
 
-			// println("" + decomposedCount + " decomposed.");
+			// Keeps looping until enough congruences are checked and
+			// decomposed.
+		} while (decomposedCount < PRIME_BASE + flattenFactor);
 
-		} while (decomposedCount < PRIME_BASE + bitChunkSize);
+		// If too many decomposed numbers are found, the topmost ones will be
+		// ignored.
+		if (decomposedCount > PRIME_BASE + flattenFactor)
+			decomposedCount = PRIME_BASE + flattenFactor;
 
-		if (decomposedCount > PRIME_BASE + bitChunkSize)
-			decomposedCount = PRIME_BASE + bitChunkSize;
-
+		// Recalculates s and f(s) according to our decomposed numbers.
 		s = new BigInteger[decomposedCount + 1];
 		fs = new BigInteger[decomposedCount + 1];
 		for (int i = 0; i < decomposedCount; i++) {
@@ -232,21 +252,25 @@ public class QS {
 		}
 		decomposedNumbers = null;
 
-		// Creating coef matrix
+		// Creates the identity matrix (flattened)
+		int id[][] = buildIdentityMatrix(decomposedCount);
+		// Creates the coef matrix
 		byte factors[][] = buildMatrix(fs, decomposedCount);
-		int identity[][] = buildIdentity(decomposedCount);
+		// Flattens the coef matrix to match the identity.
 		int matrix[][] = flattenMatrix(factors);
 
-		// Solving the linear equation.
-		solveMatrix(matrix, identity);
-		BigInteger test;
-		int loop = decomposedCount - 1;
-		do {
+		// Solving the linear equation by gauss-elimination.
+		solveMatrix(matrix, id);
+		BigInteger test = ONE;
 
+		// Loops through the decomposed vector from the top down, to find rows
+		// which correspond to quadratic congruences.
+		for (int loop = decomposedCount - 1; loop > PRIME_BASE; loop--) {
 			int primeFactors[] = new int[PRIME_BASE];
-			byte factorLine[] = extractLine(identity, loop);
+			byte factorLine[] = matrixLine(id, loop);
 
 			test = ONE;
+			// Initializes the prime factors to 0.
 			for (int i = 0; i < PRIME_BASE; i++)
 				primeFactors[i] = 0;
 			for (int i = 0; i < decomposedCount; i++) {
@@ -272,23 +296,23 @@ public class QS {
 			test = N.gcd(x);
 			if ((test.compareTo(ONE) != 0) && (test.compareTo(N) != 0))
 				break;
-		} while (--loop > PRIME_BASE);
+		}
 
 		return test;
 	}
 
-	private byte[] extractLine(int right[][], int index) {
+	private byte[] matrixLine(int matrix[][], int index) {
 		int j, k;
-		int line[] = right[index];
-		byte result[] = new byte[PRIME_BASE + bitChunkSize];
+		int line[] = matrix[index];
+		byte result[] = new byte[PRIME_BASE + flattenFactor];
 		int comparation = 1;
-		for (j = 0; j < PRIME_BASE + bitChunkSize; j++) {
-			if ((line[j / bitChunkSize] & comparation) > 0)
+		for (j = 0; j < PRIME_BASE + flattenFactor; j++) {
+			if ((line[j / flattenFactor] & comparation) > 0)
 				result[j] = 1;
 			else
 				result[j] = 0;
 
-			if (j % bitChunkSize == (bitChunkSize - 1))
+			if (j % flattenFactor == (flattenFactor - 1))
 				comparation = 1;
 			else
 				comparation *= 2;
@@ -300,17 +324,17 @@ public class QS {
 		int c1, c2;
 		int temp;
 		int comparation = 1;
-		for (c1 = 1; c1 <= (j % bitChunkSize); c1++)
+		for (c1 = 1; c1 <= (j % flattenFactor); c1++)
 			comparation *= 2;
-		if ((matrix[j][j / bitChunkSize] & comparation) == 0) {
+		if ((matrix[j][j / flattenFactor] & comparation) == 0) {
 			for (c1 = j + 1; c1 < k; c1++) {
-				if ((matrix[c1][j / bitChunkSize] & comparation) > 0) {
-					for (c2 = j / bitChunkSize; c2 < k / bitChunkSize; c2++) {
+				if ((matrix[c1][j / flattenFactor] & comparation) > 0) {
+					for (c2 = j / flattenFactor; c2 < k / flattenFactor; c2++) {
 						temp = matrix[j][c2];
 						matrix[j][c2] = matrix[c1][c2];
 						matrix[c1][c2] = temp;
 					}
-					for (c2 = 0; c2 < k / bitChunkSize; c2++) {
+					for (c2 = 0; c2 < k / flattenFactor; c2++) {
 						temp = right[j][c2];
 						right[j][c2] = right[c1][c2];
 						right[c1][c2] = temp;
@@ -320,12 +344,12 @@ public class QS {
 			}
 		}
 
-		if ((matrix[j][j / bitChunkSize] & comparation) > 0) {
+		if ((matrix[j][j / flattenFactor] & comparation) > 0) {
 			for (c1 = j + 1; c1 < k; c1++) {
-				if ((matrix[c1][j / bitChunkSize] & comparation) > 0) {
-					for (c2 = j / bitChunkSize; c2 < k / bitChunkSize; c2++)
+				if ((matrix[c1][j / flattenFactor] & comparation) > 0) {
+					for (c2 = j / flattenFactor; c2 < k / flattenFactor; c2++)
 						matrix[c1][c2] = (int) (matrix[c1][c2] ^ matrix[j][c2]);
-					for (c2 = 0; c2 < k / bitChunkSize; c2++)
+					for (c2 = 0; c2 < k / flattenFactor; c2++)
 						right[c1][c2] = (int) (right[c1][c2] ^ right[j][c2]);
 				}
 			}
@@ -365,15 +389,15 @@ public class QS {
 	}
 
 	private int[][] flattenMatrix(byte matrix[][]) {
-		int m[][] = new int[matrix.length][matrix.length / bitChunkSize];
+		int m[][] = new int[matrix.length][matrix.length / flattenFactor];
 		int j, k, n;
 		int comparation;
 		for (j = 0; j < matrix.length; j++)
-			for (k = 0; k < matrix.length / bitChunkSize; k++) {
+			for (k = 0; k < matrix.length / flattenFactor; k++) {
 				comparation = 1;
 				m[j][k] = 0;
-				for (n = 0; n < bitChunkSize; n++) {
-					if ((matrix[j][k * bitChunkSize + n] & 1) > 0)
+				for (n = 0; n < flattenFactor; n++) {
+					if ((matrix[j][k * flattenFactor + n] & 1) > 0)
 						m[j][k] += comparation;
 					comparation *= 2;
 				}
@@ -381,37 +405,38 @@ public class QS {
 		return m;
 	}
 
-	private int[][] buildIdentity(int size) {
-		int matrix[][] = new int[size][size / bitChunkSize];
-		int j, k;
-		for (j = 0; j < size; j++)
-			for (k = 0; k < size / bitChunkSize; k++)
-				matrix[j][k] = 0;
-		k = -1;
-		int comparation = 0;
-		for (j = 0; j < size; j++) {
-			if (j % bitChunkSize == 0) {
-				k++;
-				comparation = 1;
+	private int[][] buildIdentityMatrix(int size) {
+
+		int matrix[][] = new int[size][size / flattenFactor];
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size / flattenFactor; j++)
+				matrix[i][j] = 0;
+
+		int j = -1;
+		int comparison = 0;
+		for (int i = 0; i < size; i++) {
+			if (i % flattenFactor == 0) {
+				j++;
+				comparison = 1;
 			} else
-				comparation *= 2;
-			matrix[j][k] = comparation;
+				comparison *= 2;
+			matrix[i][j] = comparison;
 		}
 		return matrix;
 	}
 
-	private void removeHighestPower(int index, BigInteger p) {
+	private void divideByPower(int index, BigInteger p) {
 		do
 			fs[index] = fs[index].divide(p);
-		while (fs[index].mod(p).compareTo(ZERO) == 0);
-		if (fs[index].compareTo(ONE) == 0) {
+		while (fs[index].mod(p).equals(ZERO));
+		if (fs[index].equals(ONE)) {
 			logs[index] = 0;
 			decomposedNumbers.add(s[index]);
 			decomposedCount++;
 		}
 	}
 
-	private long[] findSmoothNumbers(long p, long n) {
+	private long[] findQxPolynomials(long p, long n) {
 		long k, x;
 
 		long result[] = new long[2];
@@ -419,43 +444,24 @@ public class QS {
 		result[1] = -1;
 
 		if (p == 2) {
-			result[0] = n % 2;
+			// Special case. The prime is 2, and n will be either 0 or 1.
+			result[0] = n;
 			result[1] = -1;
 			return result;
 		}
 
-		if (p % 4 == 3) {
-			k = (p / 4);
-			x = modPowLong(n, k + 1, p) % p;
-			result[0] = x;
-			result[1] = (p - x);
-			return result;
-		}
-
-		if (p % 8 == 5) {
-			k = (p / 8);
-			x = modPowLong(n, 2 * k + 1, p);
-			if (x == 1) {
-				x = modPowLong(n, k + 1, p);
-				result[0] = x;
-				result[1] = (p - x);
-				return result;
-			}
-			if (x == p - 1) {
-				x = modPowLong(4 * n, k + 1, p);
-				x = (x * (p + 1) / 2) % p;
-				result[0] = x;
-				result[1] = (p - x);
-				return result;
-			}
-		}
-
-		long h = 13;
+		// Find a h value for which h^2-4n is not a quadratic residue mod p.
+		long h = 3;
 		do
 			h += 2;
 		while (legendreSymbol(h * h - 4 * n, p) != -1);
 
+		// k is p/2 rounded up (p is always odd, of course).
 		k = (p + 1) / 2;
+
+		// Solves the equation of t congruent with n mod p.
+		// Algorithm from D.M. Bressoud. Factorization and Primality Testing
+		// 1989.
 		x = v_(k, h, n, p);
 		if (x < 0)
 			x += p;
